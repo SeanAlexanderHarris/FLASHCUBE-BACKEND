b1f9102e62d6538466a28b97a20c42190b7523b1
SeanAlexanderHarris <sean.alexander.harris.29@googlemail.com>
2018-05-18 13:11:28 +0100

    try graphene env

--- seed/seed.js
+++ seed/seed.js
@@ -2,11 +2,25 @@ const neo4j = require("neo4j-driver").v1;
 // const termData = require("./devData/terms");
 // const topicData = require("./devData/topics");
 // const userData = require("./devData/users");
+// const driver = neo4j.driver(
+//   "bolt://localhost",
+//   neo4j.auth.basic("neo4j", "flashcube")
+// );
+
+const {
+  GRAPHENEDB_BOLT_PASSWORD,
+  GRAPHENEDB_BOLT_URL,
+  GRAPHENEDB_BOLT_USER
+} = require("../config");
+
+const graphenedbURL = process.env.GRAPHENEDB_BOLT_URL;
+const graphenedbUser = process.env.GRAPHENEDB_BOLT_USER;
+const graphenedbPass = process.env.GRAPHENEDB_BOLT_PASSWORD;
+
 const driver = neo4j.driver(
-  "bolt://localhost",
-  neo4j.auth.basic("neo4j", "flashcube")
+  graphenedbURL,
+  neo4j.auth.basic(graphenedbUser, graphenedbPass)
 );
-const seedText = require("./");
 
 function seedDB() {
   const session = driver.session();
