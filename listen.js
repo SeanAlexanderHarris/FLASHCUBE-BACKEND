432a829b83256ecf251725db8187ad47670d2b4e
SeanAlexanderHarris <sean.alexander.harris.29@googlemail.com>
2018-05-18 22:32:44 +0100

    try node env port

--- listen.js
+++ listen.js
@@ -1,6 +1,6 @@
 const app = require("./app.js");
 
-app.listen(3000, err => {
+app.listen(process.env.PORT || 3000, err => {
   if (err) throw err;
   console.log(`Server listening on port 3000`);
 });
