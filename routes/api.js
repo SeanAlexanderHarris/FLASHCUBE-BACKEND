22dfaf2a6b5295d6be7ca955bc45e9d2d4c3da93
SeanAlexanderHarris <sean.alexander.harris.29@googlemail.com>
2018-05-18 22:28:54 +0100

    test without html on default api route

--- routes/api.js
+++ routes/api.js
@@ -3,24 +3,25 @@ const topicsRouter = require("./topics");
 const termsRouter = require("./terms");
 const usersRouter = require("./users");
 
-consolelog("4");
+console.log("4");
 
-router.get("/", function(req, res) {
-  session
-    .run("MATCH(n) RETURN n LIMIT 1")
-    .then(result => {
-      session.close();
-      console.log(result);
-      res.send({ result });
-      driver.close();
-    })
-    .catch(err => console.log);
-});
+// router.get("/", function(req, res) {
+//   session
+//     .run("MATCH(n) RETURN n LIMIT 1")
+//     .then(result => {
+//       session.close();
+//       console.log(result);
+//       res.send({ result });
+//       driver.close();
+//     })
+//     .catch(err => console.log);
+// });
 
 // on /api render a basic html menu of the routes as help for the squad
-// router.get("/", (req, res) => {
-//   res.sendFile(__dirname.replace("routes", "") + "index.html");
-// });
+router.get("/", (req, res) => {
+  // res.sendFile(__dirname.replace("routes", "") + "index.html");
+  res.send("It works?");
+});
 
 // router.use("/users", usersRouter);
 
