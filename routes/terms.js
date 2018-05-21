35239b0a832eac2a65f6378c7fe8b2fb6b50b104
SeanAlexanderHarris <sean.alexander.harris.29@googlemail.com>
2018-05-21 17:52:41 +0100

    comment out unfinished route

--- routes/terms.js
+++ routes/terms.js
@@ -8,12 +8,12 @@ router.get("/", getAllTerms);
 // get one term
 
 // get terms a user is studying
-router.get(":uid/terms", getUserTopics);
+// router.get(":uid/terms", getUserTopics);
 
 // get terms for a topic
 router.get(":topicTitle/topicterms", getTermsByTopic);
 
 // add an IS_STUDYING rel between user and term
-router.post(":uid/");
+// router.post(":uid/");
 
 module.exports = router;
