003bab7d1c9982cf7bcb2f45cc21e8a2b0c5c4ab
SeanAlexanderHarris <sean.alexander.harris.29@googlemail.com>
2018-05-21 11:25:30 +0100

    try with process env properties local to controller

--- controllers/users.js
+++ controllers/users.js
@@ -5,7 +5,19 @@ const neo4j = require("neo4j-driver").v1;
 //   neo4j.auth.basic("neo4j", "flashcube")
 // );
 // const { driver } = require("../app");
-const { session } = require("../app");
+const {
+  GRAPHENEDB_BOLT_PASSWORD,
+  GRAPHENEDB_BOLT_URL,
+  GRAPHENEDB_BOLT_USER,
+  GRAPHENEDB_URL
+} = process.env.GRAPHENEDB_URL ? process.env : require("./config/");
+
+const driver = neo4j.driver(
+  GRAPHENEDB_BOLT_URL,
+  neo4j.auth.basic(GRAPHENEDB_BOLT_USER, GRAPHENEDB_BOLT_PASSWORD)
+);
+
+// const { session } = require("../app");
 // = driver.session();
 
 exports.getAllUsers = (req, res, next) => {
